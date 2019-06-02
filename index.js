7d70a65e40abf0e151060a1c51b7c840844305aa
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-06-02 20:25:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
 
-var name = "Janet"
+var name = "Tony"
 var height = "74"
-
 
 
 console.log("Name:")
 console.log(name)
 
+console.log(parseInt(Height) + 1)
 console.log("Height:")
 console.log(height+1)
